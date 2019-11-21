ac425baba45b5089d5672ba160f93f1e71b7bd2e
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-11-21 18:10:38 +0600

    init

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+Product.destroy_all
+
+10.times do
+  p = Product.create(name: Faker::Commerce.unique.product_name,
+                     price_cents: rand(5000..100_000).to_i,
+                     description: Faker::Hipster.sentence(rand(3..6)))
+
+  puts "Creating #{p.name}"
+
+  downloaded_image = open("https://source.unsplash.com/600x600/?#{p.name}")
+  p.image.attach(io: downloaded_image, filename: "m-#{p.name}.jpg")
+end
